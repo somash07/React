99c25a74eb285b3ac27a0e8868b7bf2a0dbfe833
Somash Manandhar <manandharsomash@gmail.com>
2024-05-16 22:50:23 +0545

    useref hook

--- movie-finder/src/components/Search.js
+++ movie-finder/src/components/Search.js
@@ -1,4 +1,21 @@
+import { useRef , useEffect } from "react";
+
 export function Search({ query, setQuery }) {
+
+  const inputElement= useRef(null)
+
+  useEffect(()=>{
+    function callBack(e){
+      if(document.activeElement===inputElement.current) return
+      if(e.code==='Enter'){
+        inputElement.current.focus()
+        setQuery('')
+      }
+    }
+
+    document.addEventListener('keydown', callBack)
+  },[setQuery])
+
   //stateful
   return (
     <input
@@ -6,6 +23,6 @@ export function Search({ query, setQuery }) {
       type="text"
       placeholder="Search movies..."
       value={query}
-      onChange={(e) => setQuery(e.target.value)} />
+      onChange={(e) => setQuery(e.target.value)} ref={inputElement}/>
   );
 }
