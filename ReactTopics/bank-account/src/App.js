925b2517c5046853a77a025b9e70113919917805
Somash Manandhar <manandharsomash@gmail.com>
2024-05-25 22:01:51 +0545

    useReducer

--- ReactTopics/bank-account/src/App.js
+++ ReactTopics/bank-account/src/App.js
@@ -5,7 +5,7 @@ const initialState={
   balance: 0,
   loan: 0,
   //status can be either active or inactive for in active we just enable open acc btn but all other will be disabled
-  status : 'inactive',
+  isActive: false,
 }
 
 function reducer(state,action){
@@ -13,9 +13,34 @@ function reducer(state,action){
     case 'openAccount': 
       return {
         ...state,
-        status: 'active'
+        isActive: true,
+        balance: 500
       }
-
+    case 'deposit': 
+      return {
+        ...state,
+        balance: state.balance+ 150
+      }
+    case 'withdraw': 
+      return{
+        ...state,
+        balance: state.balance>0? state.balance-50: 0,
+      }
+    case 'loan': 
+      return {
+        ...state,
+        loan: state.loan+5000,
+      }
+    case 'payLoan': 
+      return {
+        ...state,
+        loan: state.loan>0?state.loan-5000:0
+      }
+    case 'close': 
+      return {
+        ...state,
+        isActive: (state.balance=== 0 && state.loan===0)? false: true
+      }
     default: throw new Error('unregistered event occured')
     
   }
@@ -23,7 +48,7 @@ function reducer(state,action){
 
 export default function App(){
 
-  const [{status,balance, loan}, dispatch] = useReducer(reducer, initialState)
+  const [{isActive,balance, loan}, dispatch] = useReducer(reducer, initialState)
   return <>
     <h1 align='center'>useReducer Bank Account</h1>
 
@@ -32,8 +57,16 @@ export default function App(){
        <h3>Loan: {loan}</h3>
     </Header>
 
-    <button disabled={status==='active'? true: false} onClick={()=>dispatch({type: 'openAccount'})}>Open Account</button>
+    <button disabled={isActive} onClick={()=>dispatch({type: 'openAccount'})}>Open Account</button>
 
+    <button disabled={!isActive} onClick={()=>dispatch({type: 'deposit'})}>Deposit 150</button>
 
+    <button disabled={!isActive} onClick={()=>dispatch({type: 'withdraw'})}>Withdraw 50</button>
+
+    <button disabled={!isActive} onClick={()=>dispatch({type: 'loan'})}>Request a loan of 5000</button>
+
+    <button disabled={!isActive} onClick={()=>dispatch({type: 'payLoan'})}>Pay Loan</button>
+
+    <button disabled={!isActive} onClick={()=>dispatch({type: 'close'})}>close account</button>
   </>
 }
--- react-quiz/README.md
+++ react-quiz/README.md
@@ -1,7 +1,7 @@
-## React-Quiz
+# React-Quiz
 It is a simple quiz app. This app is a practice of the useReducer hook of react. Due to the centralized state update logic in the reducer function, the complex state and multiple evt handlers are easily handled. Also the api used is based on the data from the data/questions.json and json-server is used. 
 
-##SnapShots: 
+## SnapShots: 
 
 ![alt text](image.png)
 
