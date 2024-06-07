a0c5c6441f81d81409e66ad7765c9ded17a6a2dc
Somash Manandhar <manandharsomash@gmail.com>
2024-06-07 21:05:24 +0545

    zod basics

--- ZOD-validation/vite-project/src/App.tsx
+++ ZOD-validation/vite-project/src/App.tsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { z } from "zod";
 
-function App() {
-  const [count, setCount] = useState(0)
+const UserSchema = z.object({
+  username: z.string().min(5).max(8),
+  age: z.number(),
+  birthday: z.date().optional(),
+  isProgrammer: z.boolean(),
+});
 
-  return (
-    <h1 className='underline'>hi </h1>
-  )
+type User = z.infer<typeof UserSchema>;
+
+const user = {
+  username: "somash",
+  age: 8,
+  birthday: new Date(),
+  isProgrammer: true,
+   };
+
+console.log(UserSchema.safeParse(user));
+export default function App() {
+  return <h1>hi</h1>;
 }
-
-export default App
