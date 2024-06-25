c2a2678a66b54975c078f46b94f0ed7245c82042
Somash Manandhar <manandharsomash@gmail.com>
2024-06-25 23:02:43 +0545

    refactor city component using context

--- worldwise/src/components/City.jsx
+++ worldwise/src/components/City.jsx
@@ -1,6 +1,8 @@
-import { Link, useParams, useSearchParams } from "react-router-dom";
+import { Link, useParams,  } from "react-router-dom";
 import styles from "./City.module.css";
 import { useCities } from "../context/CitiesContext";
+import { useEffect } from "react";
+import Spinner from "./Spinner"
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -11,27 +13,52 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
-  const {cities}=useCities()
-
   const {id}= useParams()
+  const {getCity, currentCity, isLoading}= useCities()
 
-  const [searchParams,setSearchParams]=useSearchParams();
-  const lat= searchParams.get('lat')
-  const lng= searchParams.get('lng')
-  // TEMP DATA
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
-
+  useEffect(()=>{
+    getCity(id)
+  },[id])
   const { cityName, emoji, date, notes } = currentCity;
 
+  if(isLoading) return <Spinner/>
+
   return <>
-  <h3>City {id}</h3>
-  <p>position: {lat},{lng}</p>
-  <Link to="/app/cities">back</Link>
+  <div className={styles.city}>
+      <div className={styles.row}>
+        <h6>City name</h6>
+        <h3>
+          <span>{emoji}</span> {cityName}
+        </h3>
+      </div>
+
+      <div className={styles.row}>
+        <h6>You went to {cityName} on</h6>
+        <p>{formatDate(date || null)}</p>
+      </div>
+
+      {notes && (
+        <div className={styles.row}>
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
+
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out {cityName} on Wikipedia &rarr;
+        </a>
+      </div>
+
+      <div>
+        
+      </div>
+    </div>
   </>
 
 }
--- worldwise/src/context/CitiesContext.jsx
+++ worldwise/src/context/CitiesContext.jsx
@@ -2,9 +2,10 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 const CitiesContext = createContext();
 
-function CitiesProvider({ children}) {
+function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(() => {
     async function cityNameFetch() {
@@ -21,19 +22,39 @@ function CitiesProvider({ children}) {
     }
     cityNameFetch();
   }, []);
-  return <CitiesContext.Provider value={{
-    cities,
-    isLoading,
-  }}>
-    {children}
-  </CitiesContext.Provider>;
+
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`http://localhost:3000/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (e) {
+      alert("error in fetch");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
 }
 
-function useCities(){
-    const context =useContext(CitiesContext);
-    if(context===undefined) throw new Error("citiesContext was used outside the cities provider")
-    return context;
 
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("citiesContext was used outside the cities provider");
+  return context;
 }
 
-export {CitiesProvider,useCities};
+export { CitiesProvider, useCities };
